84ca1c1ab9a662e76ac4e0b4cac9907c7d87417f
aminsami <aminsami91@gmail.com>
2023-12-06 10:55:42 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,34 +17,60 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        https://www.storeboard.com/dibapart
+https://www.storeboard.com/alobag1
+https://www.storeboard.com/cooltech1
+https://lwccareers.lindsey.edu/profiles/3982983-cool-tech
+https://lwccareers.lindsey.edu/profiles/3983002-diba-part
+https://lwccareers.lindsey.edu/profiles/3983013-alo-bag
+https://www.link-your-site.com/member/cooltech.html
+https://www.link-your-site.com/member/dibapart.html
+https://www.link-your-site.com/listing/business/32621-alobag.html
+http://sanaat123.rozblog.com/
+https://web.notifyninja.com/alobag.com
+https://web.notifyninja.com/dibapart.com
+https://web.notifyninja.com/cooltech.cool
+https://profiles.delphiforums.com/n/pfx/profile.aspx?nav=profile&webtag=dfpprofile000&userId=1891204216
+https://sites.google.com/view/majaleh/home
+https://www.tumblr.com/blog/cooltechhvac12
+http://www.starjiwoo.com/renewal/?document_srl=636771&mid=freeboard&page=508&rnd=2747396#comment_2747396
+http://partdiba.toonblog.ir/
+https://sanat124.monoblog.ir/
+https://fancoiliran.blogsky.com/
+https://maps.google.com/url?sa=t&url=https%3A%2F%2Falobog.com%2F
+https://maps.google.com/url?sa=t&url=https%3A%2F%2Fcooltech.cool%2F
+https://maps.google.com/url?sa=t&url=https%3A%2F%2Fballoonchi.com%2F
+https://maps.google.com/url?sa=t&url=https%3A%2F%2Fdibapart.com%2F
+https://www.youtube.com/redirect?q=alobag.com
+https://www.youtube.com/redirect?q=cooltech.cool
+https://www.youtube.com/redirect?q=dibapart.com
+https://www.youtube.com/redirect?q=balloonchi.com
+https://paper.dropbox.com/ep/redirect/external-link?url=http%3A%2F%2Falobag.com&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D
+https://paper.dropbox.com/ep/redirect/external-link?url=https%3A%2F%2Fdibapart.com&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D
+https://paper.dropbox.com/ep/redirect/external-link?url=https%3A%2F%2Fcooltech.cool&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D
+https://paper.dropbox.com/ep/redirect/external-link?url=https%3A%2F%2Fballoonchi.com&hmac=XC076%2FyQdixV26vHEOdeA%2Bm6hCzJ%2B1LuaL5SrFmv5QI%3D
+https://medium.com/r?url=https%3A%2F%2Falobag.com%2F
+https://medium.com/r?url=https%3A%2F%2Fballoonchi.com%2F
+https://medium.com/r?url=https%3A%2F%2Fdibapart.com%2F
+https://medium.com/r?url=https%3A%2F%2Fcooltech.cool%2F
+http://cr.naver.com/redirect-notification?u=https%3A%2F%2Falobag.com
+https://cr.naver.com/redirect-notification?u=https%3A%2F%2Fcooltech.cool
+http://cr.naver.com/redirect-notification?u=https%3A%2F%2Fdibapart.com
+https://cr.naver.com/redirect-notification?u=https%3A%2F%2Fballoonchi.com
+https://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Fdibapart.com
+https://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Fcooltech.cool
+https://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Falobag.com
+https://pb.lib.berkeley.edu/xtf/servlet/org.cdlib.xtf.dynaXML.DynaXML?source=/BITAGAP/Display/9775BITAGAP.Work.xml&style=Work.xsl&gobk=https%3A%2F%2Fballoonchi.com
+https://clients1.google.de/url?q=http%3A%2F%2Falobag.com%2F
+https://clients1.google.de/url?q=http%3A%2F%2Fballoonchi.com%2F
+https://clients1.google.de/url?q=http%3A%2F%2Fdibapart.com%2F
+https://clients1.google.de/url?q=http%3A%2F%2Fcooltech.cool%2F
+https://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Falobag.com
+https://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Fdibapart.com
+https://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Fballoonchi.com
+https://remote.sdc.gov.on.ca/_layouts/15/Gov.ON.LTC.SSE.Extranet.Authentication/forgotpassword.aspx?ci=en-US&sb=https%3A%2F%2Fcooltech.cool
+http://propelabu.org/w/index.php?majaleh
+
       </main>
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
